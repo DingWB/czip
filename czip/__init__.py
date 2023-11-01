7bede88990d19063c211b4f27a11eda5c2c024ac
DingWB <ding.wu.bin.gm@gmail.com>
2023-11-01 15:24:39 -0700

    update to 0.2
    Signed-off-by: DingWB <ding.wu.bin.gm@gmail.com>

--- czip/__init__.py
+++ czip/__init__.py
@@ -8,7 +8,7 @@ from .cz import (
 from .allc import (AllC, generate_ssi, bed2cz,
                    merge_cz, extractCG, merge_cell_type)
 
-__version__ = "0.1"
+__version__ = "0.2"
 
 
 def main():
